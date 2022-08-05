be61fafd059667efae2e787374dce30a9d0d3d4b
Raoul Hecky <raoul.hecky@gmail.com>
2022-08-05 16:37:37 +0200

    add app icon

--- android/app/src/main/kotlin/com/example/my_greenhouse/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.my_greenhouse
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/fr/calaos/my_greenhouse/MainActivity.kt
@@ -0,0 +1,6 @@
+package fr.calaos.my_greenhouse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
